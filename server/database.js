886a78e5052dc13341629fed1a1894e5e68d2601
said <saidkourbisse@gmail.com>
2024-09-17 00:30:28 +0100

    add variables

--- server/database.js
+++ server/database.js
@@ -1,9 +1,12 @@
+require("dotenv").config(); // Add this line if you're using a .env file
 const mongoose = require("mongoose");
 
+const uri = process.env.DATABASE_PROD;
+if (!uri) {
+  throw new Error("DATABASE is not set in .env file");
+}
 mongoose
-  .connect(
-    "mongodb+srv://saidkourbisse:uPx2ENOND3bqUmj7@cluster0.3yaac.mongodb.net/"
-  )
+  .connect(uri)
   .then(() => {
     console.log(`Database connected`);
   })
