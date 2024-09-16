9fb1ee05496191b3b7cfda604f0d3dc76563d34b
said <saidkourbisse@gmail.com>
2024-09-17 00:23:23 +0100

    update db

--- server/database.js
+++ server/database.js
@@ -2,9 +2,9 @@ require("dotenv").config(); // Add this line if you're using a .env file
 const mongoose = require("mongoose");
 
 const uri = process.env.DATABASE_PROD;
-if (!uri) {
-  throw new Error("DATABASE is not set in .env file");
-}
+// if (!uri) {
+//   throw new Error("DATABASE is not set in .env file");
+// }
 mongoose
   .connect(
     "mongodb+srv://saidkourbisse:uPx2ENOND3bqUmj7@cluster0.3yaac.mongodb.net/"
